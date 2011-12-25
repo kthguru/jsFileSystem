24e4f836ef470375d9e0eba7a08e0da3625b422f
LCID Fire <lcid-fire@gmx.net>
2011-12-25 20:16:15 +0100

    Make sure JS file operates in strict mode

--- coffee.py
+++ coffee.py
@@ -5,6 +5,8 @@ import subprocess
 jsoutput = subprocess.Popen(['coffee', '-cp', 'filesystem.coffee'], stdout=subprocess.PIPE).communicate()[0]
 jsfile = open('filesystem.js', 'w')
 
+jsfile.write('"use strict";\n')
+
 for line in jsoutput.split('\n'):
 	line.replace('getter', 'get ')
 	print line
